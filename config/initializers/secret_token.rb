c156f1a28aa6fa3a2256a72a876be9d58b610a3f
Rohan Mitchell <rohan@rohanmitchell.com>
2013-02-16 12:28:43 +1100

    Rails 3.2.11

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogaday::Application.config.secret_token = 'b3f51442975d7ba95597f3be64a4a029a66c79a68daadfa2f3779e676f777d3bf4c0c59b7ed5842ab759a656b9c84273774b81d69291950eba62deb47baab89c'
