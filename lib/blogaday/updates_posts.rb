ea61ee3d6f42cb9587c6e91a481f9d5ca16c5630
Rohan Mitchell <rohan@rohanmitchell.com>
2013-02-16 14:00:42 +1100

    Add any posts that aren't in the db

--- lib/blogaday/updates_posts.rb
+++ lib/blogaday/updates_posts.rb
@@ -1,6 +1,25 @@
 module Blogaday
   class UpdatesPosts
-    def self.go!
+    def go!
+      # Add any posts that aren't in the db
+      posts.each do |post|
+        add_post post unless post_exists? post
+      end
+    end
+
+
+    private
+
+    def posts
+      @posts ||= FetchesPosts.go!
+    end
+
+    def add_post(post)
+      Post.create! title: post.title, url: post.url
+    end
+
+    def post_exists?(post)
+      Post.where(title: post.title, url: post.url).present?
     end
   end
 end
